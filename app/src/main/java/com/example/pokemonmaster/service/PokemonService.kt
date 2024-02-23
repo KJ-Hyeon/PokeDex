1e4f4ba57b40944ff8d60594634927530bf4f624
KJ-Hyeon <bark052526@gmail.com>
2024-02-23 19:36:48 +0900

    feat: set foreground service

---
+++ app/src/main/java/com/example/pokemonmaster/service/PokemonService.kt
@@ -0,0 +1,129 @@
+package com.example.pokemonmaster.service
+
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import com.example.pokemonmaster.R
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+class PokemonService: Service() {
+    private val LOCATION_SERVICE_ID = 1000
+    private val channelId = "location_channel"
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            if (locationResult.lastLocation != null) {
+                val latitude = locationResult.lastLocation?.latitude
+                val longitude = locationResult.lastLocation?.longitude
+                Log.d("PokemonService:","PokemonService: $latitude, $longitude")
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null // 바운드 서비스가 아니기 때문에 null을 return
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            val action = intent.action
+            action?.let { action ->
+                when(action) {
+                    "startLocationService" -> startLocationService()
+                    "stopLocationService" -> stopLocationService()
+                }
+            }
+        }
+//        서비스가 강제로 종료되었을 때 시스템에 재시작을 요청
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            channelId,
+            "Location Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = "Location Service Channel"
+            setShowBadge(true)
+        }
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+    }
+
+    private fun createNotification(): Notification {
+        val resultIntent = Intent()
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(this, channelId).apply {
+            setSmallIcon(R.drawable.item_pokemon_ball) // 작은 아이콘 설정
+            setContentTitle("Location Service") // 알림의 제목
+            setDefaults(NotificationCompat.DEFAULT_ALL) // 사운드, 진동 및 라이트를 포함한 모든 기본 알림 속성
+            setContentText("Run") // 본문 텍스트
+            setContentIntent(pendingIntent) // 알림이 터치 되었을 경우, 실행 될 pendingIntent 설정
+            setAutoCancel(false) // 알림을 터치해도 자동으로 취소가 되지않는다.
+            priority = NotificationCompat.PRIORITY_MAX // 알림의 중요도를 설정 PRIORITY_MAX
+        }
+
+        return builder.build()
+    }
+
+    private fun startLocationService() {
+        //이렇게 설정된 LocationRequest는 4초마다 위치 업데이트를 요청하며, 가능한 한 빠르게(2초 간격) 정확한 위치 정보를 얻으려고 합니다.
+        val locationRequest = LocationRequest.create()
+            .setInterval(4000) // 4초마다 한번씩 위치 업데이트를 요청한다..
+            .setFastestInterval(2000) //  가장 빠른 위치 업데이트의 간격을 설정
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) // 높은 정확도의 위치 업데이트를 요청한다.
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+            // 위치 업데이트를 요청하는 메서드
+            .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        createNotificationChannel()
+        //         foreground 서비스를 시작 , 파라미터는 notification을 위한 요소들
+        startForeground(LOCATION_SERVICE_ID, createNotification()) // setNotification의 builder가 들어가야함
+    }
+
+    private fun stopLocationService() {
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(locationCallback)
+        stopForeground(STOP_FOREGROUND_REMOVE) // foreground 서비스 종료 및 노티피케이션 제거
+        stopSelf() // 서비스 종료
+    }
+}
--- app/src/main/java/com/example/pokemonmaster/ui/map/PokeMapFragment.kt
+++ app/src/main/java/com/example/pokemonmaster/ui/map/PokeMapFragment.kt
@@ -1,13 +1,20 @@
 package com.example.pokemonmaster.ui.map
 
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.pokemonmaster.MainActivity
 import com.example.pokemonmaster.R
 import com.example.pokemonmaster.databinding.FragmentMapBinding
+import com.example.pokemonmaster.service.PokemonService
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
@@ -17,7 +24,6 @@ import com.naver.maps.map.util.FusedLocationSource
 class PokeMapFragment : Fragment(), OnMapReadyCallback {
     private val binding: FragmentMapBinding by lazy { FragmentMapBinding.inflate(layoutInflater) }
     private val mapFragment: MapFragment by lazy { childFragmentManager.findFragmentById(R.id.naver_map) as MapFragment }
-    private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
@@ -31,6 +37,7 @@ class PokeMapFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         mapFragment.getMapAsync(this)
 //        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        startLocationService()
     }
 
     override fun onMapReady(naverMap: NaverMap) {
@@ -38,6 +45,43 @@ class PokeMapFragment : Fragment(), OnMapReadyCallback {
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
         naverMap.locationSource = (activity as MainActivity).locationSource
         binding.btnNaverLocation.map = naverMap
+    }
+
+    private fun isLocationServiceRunning(): Boolean {
+        // 현재 액티비티의 시스템 매니저를 가져온다.
+        val activityManager = requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+        activityManager?.let {
+            // 현재 실행 중인 애플리케이 프로세슴 목록을 가져온다.
+            val runningAppProcess = activityManager.runningAppProcesses
+            for (processInfo in runningAppProcess) {
+                // 현재 순회 중인 프로세스의 이름이 일치하면 해당 서비스가 실행 중임을 나타낸다.
+                if (processInfo.processName == "com.example.pokemonmaster:service.PokemonService") return true
+            }
+        }
+        return false
+    }
+
+    private fun startLocationService() {
+        if (!isLocationServiceRunning()) {
+            val intent = Intent(requireActivity().applicationContext, PokemonService::class.java)
+            intent.action = "startLocationService"
+            requireActivity().startForegroundService(intent)
+            Toast.makeText(requireContext(), "Location service started", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun stopLocationService() {
+        if (isLocationServiceRunning()) {
+            val intent = Intent(requireActivity().applicationContext, PokemonService::class.java)
+            intent.action = "stopLocationService"
+            requireActivity().startForegroundService(intent)
+            Toast.makeText(requireContext(), "Location service stopped", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("PokeMapFragment:","PokeMapFragment: onDestroy")
     }
 
     companion object {
